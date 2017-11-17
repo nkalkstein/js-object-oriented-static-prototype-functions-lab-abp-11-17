b2dad9e5154a47d9fcaf313619df6cc707ea483d
Noah Kalkstein <nkalkstein@gmail.com>
2017-11-17 02:05:39 +0000

    automatically backed up by learn

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -1,4 +1,14 @@
 function IcebreakerResponse(email){
   this.email = email
+  function User(name, email){
+  this.name = name
+  this.email = email
 }
+ 
+IcebreakerResponse.BatchCreate = function(data){
+  return data.map(function(icebreakerResponseData){
+    return new IcebreakerResponse(icebreakerResponseData[0], icebreakerResponseData[1])
+  })
+}
+
 
